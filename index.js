c0ba3df5f193d0e40cd1f511e864ed96611b4ccd
ahmed ismail <ahmed.ismail.fintech@gmail.com>
2023-05-16 14:43:55 +0300

    second commit

--- index.js
+++ index.js
@@ -18,3 +18,30 @@ function handleIntersection(entries, options) {
 const observer = new IntersectionObserver(handleIntersection);
 
 observer.observe(target);
+
+let per_page = 10;
+let total = 100;
+const percent_element = document.querySelector(".percent-scroll");
+const load_more_button = document.querySelector(".load-more");
+const per_page_element = document.querySelector(".per_page");
+const total_element = document.querySelector(".total");
+
+function handlePercent() {
+  let percent = (per_page / total) * 100;
+  percent_element.style.width = `${percent}%`;
+  per_page_element.textContent = per_page;
+  total_element.textContent = total;
+}
+
+load_more_button.addEventListener("click", () => {
+  if (per_page < total) {
+    per_page += 10;
+  } else {
+    document.querySelector(".scroll-bar").style.display = "none";
+    document.querySelector(".show-page-count").style.display = "none";
+    load_more_button.style.display = "none";
+  }
+
+  handlePercent();
+});
+handlePercent();
